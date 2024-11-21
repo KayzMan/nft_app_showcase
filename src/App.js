062a5c395cc4c4618f6194e79b293e639b19dc80
Ngoni Katsidzira <ngonik8@gmail.com>
2024-11-21 13:44:32 +0200

    Implemented landing page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Download, Features, SectionWrapper } from "./components";
+
+import assets from "./assets";
+import styles from "./styles/Global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing."
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImage={assets.homeHero}
+        banner="banner"
+      />
+
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProfNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImage={assets.homeCards}
+        reverse
+      />
+
+      <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProfNet is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands."
+        mockupImage={assets.feature}
+        reverse={true}
+      />
+
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. the first screens lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImage={assets.mockup}
+        banner={"banner02"}
+      />
+
+      <Download />
+
+      <div className={`px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04`}>
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className={`bold`}>Ngoni Katsidzira</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
